e55d28ac2366439e1c18c9fd0bfb6b1bb3165635
Maurits Lamers <maurits@weidestraat.nl>
2012-05-18 00:20:26 +0200

    now working properly after adding running status

--- midi.js
+++ midi.js
@@ -35,38 +35,51 @@ var readEvent = function(buffer,offset){
   // first read byte
   var byte, ret;
   var delta = readVariableLength(buffer,offset);
-  util.log('delta = ' + delta.value);
+  util.log('reading event at offset: ' + offset.toString(16));
+  util.log('delta = ' + util.inspect(delta));
   var pos = delta.length;
   byte = buffer.readUInt8(offset + pos);
   util.log('event type byte reads: ' + byte);
   if(byte === 0xFF){ // meta
+    util.log('event is meta');
     ret = metaEvent(buffer,offset+pos);
   } else if(byte === 0xF0){ // sysex
+    util.log('event is sysex');
     ret = sysexEvent(buffer,offset+pos); 
   } else {
+    util.log('event is midicontrol');
     ret = midicontrolEvent(buffer,offset+pos);
   }
   ret['delta'] = delta.value;
+  ret.length += delta.length; 
   return ret;
   //return noteEvent(buffer,offset+pos);
 };
 
 var readTrack = function(buffer,offset){
-  var events = [], chunksize, pos, evt;
+  var events = [], chunksize, pos, evt, ret;
   if(!buffer) return;
+  var trackh = buffer.toString('utf8',offset,offset + 4);
+  util.log('trackh: ' + trackh);
   if(buffer.toString('utf8',offset,offset + 4) !== "MTrk") return;
   chunksize = buffer.readUInt32BE(offset + 4); // number of bytes of trackdata starting next
-  pos = 8;
-  while(pos<chunksize){
-    evt = readEvent(buffer,offset+pos);
-    pos += evt.length;
-    events.push(evt);
-  }
-  util.log('events: ' + util.inspect(events));
-  return {
+  util.log('chunksize = ' + chunksize);
+  ret = {
     events: events,
     length: chunksize+8
   };
+  
+  pos = 8;
+  while(pos<chunksize+8){
+  // for(var i=0;i<8;i+=1){
+    evt = readEvent(buffer,offset+pos);
+    util.log('event read is: ' + util.inspect(evt));
+    pos += evt.length;
+    events.push(evt);
+    //if(pos >= chunksize) return ret;
+  }
+  //util.log('events: ' + util.inspect(events));
+  return ret;
 };
 
 
@@ -77,11 +90,16 @@ fs.readFile('simon2.mid',function(err,content){
   util.log('header info is: ' + util.inspect(header));
   var tracks = [], track;
   var pos = 14;
-  for(var i=0;i<header.numTracks;i+=1){
-    track = readTrack(content,pos); // header size is always 14
-    pos += track.length;
-    tracks.push(track);
-  }
+  track = readTrack(content,pos);
+  track = readTrack(content,pos+track.length);
+  // for(var i=0;i<header.numTracks;i+=1){
+  //   
+  //   track = readTrack(content,pos); // header size is always 14
+  //   //util.log('track ' + i + ': '  + util.inspect(track));
+  //   pos += track.length;
+  //   //util.log('new pos value: ' + pos);
+  //   tracks.push(track);
+  // }
   //util.log('tracks: ' + util.inspect(tracks));
   
 });
