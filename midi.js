eab4d2f53a17b20389ab46e64a60c969375b2c62
Maurits Lamers <maurits@weidestraat.nl>
2012-05-18 13:34:44 +0200

    commenting out util.logs

--- midi.js
+++ midi.js
@@ -35,19 +35,19 @@ var readEvent = function(buffer,offset){
   // first read byte
   var byte, ret;
   var delta = readVariableLength(buffer,offset);
-  util.log('reading event at offset: ' + offset.toString(16));
-  util.log('delta = ' + util.inspect(delta));
+  //util.log('reading event at offset: ' + offset.toString(16));
+  //util.log('delta = ' + util.inspect(delta));
   var pos = delta.length;
   byte = buffer.readUInt8(offset + pos);
-  util.log('event type byte reads: ' + byte);
+  //util.log('event type byte reads: ' + byte);
   if(byte === 0xFF){ // meta
-    util.log('event is meta');
+    //util.log('event is meta');
     ret = metaEvent(buffer,offset+pos);
   } else if(byte === 0xF0){ // sysex
-    util.log('event is sysex');
+    //util.log('event is sysex');
     ret = sysexEvent(buffer,offset+pos); 
   } else {
-    util.log('event is midicontrol');
+    //util.log('event is midicontrol');
     ret = midicontrolEvent(buffer,offset+pos);
   }
   ret['delta'] = delta.value;
@@ -60,10 +60,10 @@ var readTrack = function(buffer,offset){
   var events = [], chunksize, pos, evt, ret;
   if(!buffer) return;
   var trackh = buffer.toString('utf8',offset,offset + 4);
-  util.log('trackh: ' + trackh);
+  //util.log('trackh: ' + trackh);
   if(buffer.toString('utf8',offset,offset + 4) !== "MTrk") return;
   chunksize = buffer.readUInt32BE(offset + 4); // number of bytes of trackdata starting next
-  util.log('chunksize = ' + chunksize);
+  //util.log('chunksize = ' + chunksize);
   ret = {
     events: events,
     length: chunksize+8
@@ -73,7 +73,7 @@ var readTrack = function(buffer,offset){
   while(pos<chunksize+8){
   // for(var i=0;i<8;i+=1){
     evt = readEvent(buffer,offset+pos);
-    util.log('event read is: ' + util.inspect(evt));
+    //util.log('event read is: ' + util.inspect(evt));
     pos += evt.length;
     events.push(evt);
     //if(pos >= chunksize) return ret;
@@ -83,23 +83,46 @@ var readTrack = function(buffer,offset){
 };
 
 
-fs.readFile('simon2.mid',function(err,content){
-  // content is a Buffer
+var readMidiFromBuffer = function(content){
   var header = readMidiHeader(content);
-  if(!header.isMidi) throw('Not a midi file!');
-  util.log('header info is: ' + util.inspect(header));
+  if(!header.isMidi) return;
+  //util.log('header info is: ' + util.inspect(header));
   var tracks = [], track;
   var pos = 14;
-  track = readTrack(content,pos);
-  track = readTrack(content,pos+track.length);
-  // for(var i=0;i<header.numTracks;i+=1){
-  //   
-  //   track = readTrack(content,pos); // header size is always 14
-  //   //util.log('track ' + i + ': '  + util.inspect(track));
-  //   pos += track.length;
-  //   //util.log('new pos value: ' + pos);
-  //   tracks.push(track);
-  // }
+  for(var i=0;i<header.numTracks;i+=1){ 
+    track = readTrack(content,pos); // header size is always 14
+    //util.log('track ' + i + ': '  + util.inspect(track));
+    pos += track.length;
+    //util.log('new pos value: ' + pos);
+    tracks.push(track);
+  }
   //util.log('tracks: ' + util.inspect(tracks));
-  
-});
+};
+
+exports.readMidiFile = function(filename,callback){ // callback gets err,array_with_tracks
+
+  fs.readFile(filename,function(err,content){
+    // content is a Buffer
+    if(err){
+      callback(err);
+      return;
+    } 
+    var ret = readMidiFromBuffer(content);
+    if(!ret) callback(new Error("Not a midi file"));
+
+
+    callback(null,ret);
+  });
+};
+
+exports.readMidiFileSync = function(filename){
+  var content = fs.readFileSync(filename);
+  if(content){
+    var ret = readMidiFromBuffer(content);
+    if(!ret) throw(new Error("Not a midifile"));
+    else return ret;
+  }
+};
+
+
+
